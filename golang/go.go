297eecd623c1eb0210813f27e5b2330b15826d51
jiujuan <jiujuanfeng@163.com>
2019-07-30 19:04:07 +0800

    go

---
+++ golang/go.go
@@ -0,0 +1,11 @@
+// You can edit this code!
+// Click here and start typing.
+// https://golang.org/
+// https://blog.golang.org/
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, 世界")
+}
